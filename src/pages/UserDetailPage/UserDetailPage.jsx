5d6685ca2b106697605be92b2e196fae1c108d69
panderonak <pandeyronak139@gmail.com>
2024-12-08 14:25:39 +0530

    feat: add UserDetailPage to render UserDetail component with centered layout

---
+++ src/pages/UserDetailPage/UserDetailPage.jsx
@@ -0,0 +1,9 @@
+import { UserDetail } from "../../components";
+
+export default function UserDetailPage() {
+  return (
+    <div className="w-full h-full py-32 flex items-center justify-center">
+      <UserDetail />
+    </div>
+  );
+}
