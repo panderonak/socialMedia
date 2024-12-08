14b82ee202dcf554cbd1998f30b19f31ec58eb9e
panderonak <pandeyronak139@gmail.com>
2024-12-08 16:29:08 +0530

    feat: add ProfilePage to display Profile component

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,9 @@
+import { Profile } from "../../components";
+
+export default function ProfilePage() {
+  return (
+    <div className="py-5 w-full h-screen flex items-center justify-center">
+      <Profile />
+    </div>
+  );
+}
